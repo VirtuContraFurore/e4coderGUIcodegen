60943edd62ae88d9ffa6b27652eee5b6d7f82da0
Riccardo Mancini <r.mancini@santannapisa.it>
2020-05-02 13:30:00 +0200

    Call WM_SI_drawPixel in dummy_draw

--- windowmanager/widget/dummy.c
+++ windowmanager/widget/dummy.c
@@ -7,9 +7,10 @@ struct WidgetFunctions dummy_WidgetFunctions = {
     .onTouch = &dummy_onTouch,
 };
 
-
 void dummy_draw(struct Widget *self){
-    LOG_INFO("Dummy draw");
+    LOG_INFO("Call dummy draw");
+    WM_SI_drawPixel((struct Point){0,0}, RED);
+    LOG_INFO("Exit dummy draw");
 }
 
 bool dummy_onTouch(struct Widget *self, struct TouchEvent *event){
