e82fe453061d76e352bf4d46aae497b59384422f
ayoub el ourrak <43897325+ayoubelourrak@users.noreply.github.com>
2020-05-06 01:23:22 +0200

    Update button.c

--- windowmanager/src/widget/button.c
+++ windowmanager/src/widget/button.c
@@ -3,19 +3,19 @@
 void buttonDraw(struct Widget *self){
     struct button_Widget* data = (struct button_Widget*) self->data;
     
-      WM_SCRIF_drawBitmap({*data->dim.x,*data->dim.y}, *data->Image);
+      WM_SCRIF_drawBitmap({data->dim.x,data->dim.y}, data->Image);
 }
 
 bool buttonOnTouch(struct Widget *self, struct TouchEvent *event){
-    struct button_Widget data = (struct button_Widget*) self->data;
+    struct button_Widget* data = (struct button_Widget*) self->data;
     
     if (event->type == SINGLE_TOUCH){
         struct SingleTouchData* touch_data = (struct SingleTouchData*) event->event_data;
 
         if (touch_data->type == TOUCH_DOWN){
-            if (pointInRect(touch_data->p, *data->dim)){
+            if (pointInRect(touch_data->p, data->dim)){
                 if (data->onTouch != NULL)
-                    return *data->onTouch(self, event);
+                    return data->onTouch(self, event);
                 else
                     return false;
             }
