958e036f0e03f60c84a93efee8b612fc5f8b6293
ayoub el ourrak <43897325+ayoubelourrak@users.noreply.github.com>
2020-05-05 17:16:03 +0200

    Update button.h

--- windowmanager/include/windowmanager/widget/button.h
+++ windowmanager/include/windowmanager/widget/button.h
@@ -7,20 +7,20 @@
 #include "windowmanager/core/touch.h"
 #include "windowmanager/utility/geometry.h"
 
-struct button_widget{
+struct button_Widget{
 	struct Rect dim;
 	bool state;
 	struct Bitmap onImage;
 	struct Bitmap offImage;
-	void (*value_changed_callback)(struct widget *self, bool new_value, bool old_value);
+	void (*valueChangedCallback)(struct Widget *self, bool new_value, bool old_value);
 };
 
-void button_draw(struct Widget *self);
-void button_on_touch(struct Widget *self, struct TouchEvent type, void * event_data);
+void buttonDraw(struct Widget *self);
+void buttonOnTouch(struct Widget *self, struct TouchEvent type, void * event_data);
 
-struct WidgetFunctions button_WidgetFunctions = {
-    .draw = &button_draw,
-    .on_touch = &button_on_touch,
+struct WidgetFunctions buttonWidgetFunctions = {
+    .draw = &buttonDraw,
+    .on_touch = &buttonOnTouch,
 };
 
 #endif
