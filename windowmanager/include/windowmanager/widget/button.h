a59580cbcbd03f678a52c7dfd26eb23a64a17f8c
ayoub el ourrak <43897325+ayoubelourrak@users.noreply.github.com>
2020-05-05 17:04:15 +0200

    Update button.h

--- windowmanager/include/windowmanager/widget/button.h
+++ windowmanager/include/windowmanager/widget/button.h
@@ -9,21 +9,18 @@
 
 struct button_widget{
 	struct Rect dim;
-	bool visible;
 	bool state;
-	bitmap onImagePath;
-	bitmap offImagePath;
+	struct Bitmap onImage;
+	struct Bitmap offImage;
 	void (*value_changed_callback)(struct widget *self, bool new_value, bool old_value);
 };
 
-void button_draw(struct widget *self);
-void button_on_touch(struct widget *self, touch_event type, void * event_data);
-void button_check_boundaries(struct widget *self, touch_event type, void * event_data);
+void button_draw(struct Widget *self);
+void button_on_touch(struct Widget *self, struct TouchEvent type, void * event_data);
 
-struct widget_func button_widget_func = {
+struct WidgetFunctions button_WidgetFunctions = {
     .draw = &button_draw,
     .on_touch = &button_on_touch,
-    .check_bounaries = &button_check_boundaries
 };
 
 #endif
