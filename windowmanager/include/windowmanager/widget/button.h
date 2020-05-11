47117a7262da217f24c7a27eb8132c5819861748
Ayoub <ayoubelourrak@protonmail.com>
2020-05-11 16:57:45 +0200

    ok

---
+++ windowmanager/include/windowmanager/widget/button.h
@@ -0,0 +1,24 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "stdbool.h"
+
+#include "windowmanager/widget/widget.h"
+#include "windowmanager/core/touch.h"
+#include "windowmanager/utility/geometry.h"
+
+struct button_Widget{
+	struct Rect dim;
+	struct Bitmap Image;
+	void (*valueChangedCallback)(struct Widget *self, bool new_value, bool old_value);
+};
+
+void buttonDraw(struct Widget *self);
+bool buttonOnTouch(struct Widget *self, struct TouchEvent *type);
+
+struct WidgetFunctions buttonWidgetFunctions = {
+    .draw = &buttonDraw,
+    .onTouch = &buttonOnTouch,
+};
+
+#endif
