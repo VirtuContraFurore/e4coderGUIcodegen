8f1850cdccf6a4db68a43cb227f51a99c71ecb45
luca <luca@FeniceCeleste>
2020-08-15 23:59:54 +0200

    image tmp wip

--- codegen/it.sssuprojects.e4codergui.codegen/src/it/sssuprojects/e4codergui/codegen/services/ImageConversion.java
+++ codegen/it.sssuprojects.e4codergui.codegen/src/it/sssuprojects/e4codergui/codegen/services/ImageConversion.java
@@ -14,4 +14,24 @@ public class ImageConversion {
 		File f = new File(path.getPath());
 		System.out.println(f.exists());
 	}
+	
+	//colore rgb 565
+	public static String getBitmap() {
+		return "0xFF";
+	}
+	
+	//1->presente 0->assente
+	public static String getAlpha() {
+		return "0x00";
+	}
+	
+	public static int getWidth() {
+		return 0;
+	}
+	
+	public static int getHeight() {
+		return 0;
+	}
+	
+	
 }
